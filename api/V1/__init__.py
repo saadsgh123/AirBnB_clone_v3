7f2d3bf635f1ae4a2cb1f8ddbfb1fc9b50824a16
saadsgh123 <sdsghouri@gmail.com>
2024-08-06 23:13:25 +0100

    task 3 try 1

---
+++ api/V1/__init__.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+"""sharing app_views Blueprint"""
+
+from flask import Blueprint
+app_views = Blueprint('app_views', __name__)
---
+++ api/V1/index.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""define routes of blueprint
+"""
+
+from api.v1.views import app_views
+
+@app_views.route("/status", strict_slashes=False, methods=["GET"])
+def status():
+    return {
+        "status": "OK",
+    }
